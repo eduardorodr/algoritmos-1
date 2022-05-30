519c98ab252d123b10b443a5d2d6ae5149d0e10e
Eduardo <eduardo.rodrigues2003@unesp.br>
2022-05-30 08:52:03 -0300

    exercicios dias 11-05 e 16-05

--- modelo.c
+++ modelo.c
@@ -1,8 +1,24 @@
+/*
+*
+* 	MODELO PARA EXERCICIOS DE ALGORITMOS
+* 	EDUARDO RODRIGUES
+*
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+#include <locale.h>
+#include <math.h>
+#define pi 3.14
 
-int main(){
-
-
+main()
+{
+	
+	setlocale(LC_ALL,"Portuguese");
+	srand(time(NULL));
+	
 	return 0;
+	
 }
-
